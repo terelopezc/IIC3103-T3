c063e911f925e3153b0682d628b0399d269d1d38
Teresita Lopez <tlpc@uc.cl>
2023-05-18 22:32:08 -0400

    Inicio

--- app.js
+++ app.js
@@ -2,58 +2,139 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// Agrega el middleware para procesar el cuerpo de la solicitud como JSON
+app.use(express.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+let cantidadOperaciones = 0;
+const mensajes = [];
 
+function decodificarMensaje(base64) {
+  const decodedData = Buffer.from(base64, 'base64').toString('utf-8');
+  const tipoOperacion = decodedData.substring(0, 4);
+  const idMensaje = decodedData.substring(4, 14);
+  const bancoOrigen = decodedData.substring(14, 21).padStart(7, '0');
+  const cuentaOrigen = decodedData.substring(21, 31).padStart(10, '0');
+  const bancoDestino = decodedData.substring(31, 38).padStart(7, '0');
+  const cuentaDestino = decodedData.substring(38, 48).padStart(10, '0');
+  const monto = decodedData.substring(48).padStart(16, '0');
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  return {
+    tipoOperacion,
+    idMensaje,
+    bancoOrigen,
+    cuentaOrigen,
+    bancoDestino,
+    cuentaDestino,
+    monto,
+  };
+}
+
+// Ruta POST para recibir los mensajes de Pub/Sub
+app.post("/", (req, res) => {
+  const message = req.body.message;
+  // Aquí puedes realizar acciones basadas en el mensaje recibido
+  console.log("Mensaje recibido:", message);
+  cantidadOperaciones++;
+  const decodedMessage = decodificarMensaje(message.data);
+  mensajes.push(decodedMessage);
+  res.status(200).send("Mensaje recibido");
+});
+
+// Ruta GET para mostrar la página HTML con la tabla de mensajes
+app.get("/", (req, res) => {
+  let tableRows = '';
+  for (const mensaje of mensajes) {
+    tableRows += `
+      <tr>
+        <td>${mensaje.tipoOperacion}</td>
+        <td>${mensaje.idMensaje}</td>
+        <td>${mensaje.bancoOrigen}</td>
+        <td>${mensaje.cuentaOrigen}</td>
+        <td>${mensaje.bancoDestino}</td>
+        <td>${mensaje.cuentaDestino}</td>
+        <td>${mensaje.monto}</td>
+      </tr>
+    `;
+  }
+
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Transacciones TereBank</title>
+        <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+        <script>
+          setTimeout(() => {
+            confetti({
+              particleCount: 100,
+              spread: 70,
+              origin: { y: 0.6 },
+              disableForReducedMotion: true
+            });
+          }, 500);
+        </script>
+        <style>
+          @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+          @font-face {
+            font-family: "neo-sans";
+            src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+            font-style: normal;
+            font-weight: 700;
+          }
+          html {
+            font-family: neo-sans;
+            font-weight: 700;
+            font-size: 12px; /* Tamaño de fuente ajustado */
+          }
+          body {
+            background: white;
+            margin: 0;
+            padding: 0;
+          }
+          section {
+            border-radius: 1em;
+            padding: 1em;
+            margin: 1em;
+          }
+          /* Estilos de la tabla */
+          table {
+            border-collapse: collapse;
+            width: 100%;
+            font-size: 12px; /* Tamaño de fuente ajustado */
+            text-align: left;
+          }
+
+          th, td {
+            padding: 8px;
+            vertical-align: top;
+          }
+
+          th {
+            background-color: #f2f2f2;
+          }
+        </style>
+      </head>
+      <body>
+        <section>
+          <h1>Transacciones TereBank</h1>
+          <table>
+            <tr>
+              <th>Tipo de Operación</th>
+              <th>ID de Mensaje</th>
+              <th>Banco de Origen</th>
+              <th>Cuenta de Origen</th>
+              <th>Banco de Destino</th>
+              <th>Cuenta de Destino</th>
+              <th>Monto</th>
+            </tr>
+            ${tableRows}
+          </table>
+        </section>
+      </body>
+    </html>
+  `;
+
+  res.type('html').send(html);
+});
+
+app.listen(port, () => console.log(`La aplicación está escuchando en el puerto ${port}!`));
